c280da81e4a711954f62f53701352b58f8b95f4b
Melinda-EM <melinda.esteves-mendes@epitech.eu>
2023-03-26 21:52:10 +0200

    modif

--- src/pages/Artist.js
+++ src/pages/Artist.js
@@ -1,7 +1,26 @@
-const Artist = () => {
-    return (
-        <div>Artist component</div>
-    )
+import React, { useState, useEffect } from 'react';
+
+function Artist() {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/artists')
+      .then(response => response.json())
+      .then(data => setArtists(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>List of Artists</h2>
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>
+            {artist.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Artist;
